fcb4e64c096241daecc04ecc0fa7e6c70d2581bc
tony <tony20191702013@cis.asu.edu.eg>
2022-12-26 00:42:47 +0200

    added cloud db feature  after adding to local db

--- app/src/main/java/com/project/listapp/AddNewTaskActivity.java
+++ app/src/main/java/com/project/listapp/AddNewTaskActivity.java
@@ -58,82 +58,45 @@ public class AddNewTaskActivity extends AppCompatActivity {
         TaskTitleTxt=(EditText)findViewById(R.id.TaskNameTxt);
         NoteTxt=(EditText)findViewById(R.id.NoteTxt);
 
-       /* mAuth=FirebaseAuth.getInstance();
-        mFirebaseDb=FirebaseDatabase.getInstance();
-        myRef=mFirebaseDb.getReference("note");*/
-
-
-     /*   mAuthListener=new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user= firebaseAuth.getCurrentUser();
-                if(user!=null)
-                {
-                    Log.d(TAG, "onAuthStateChanged: signed in "+user.getUid());
-                    Toast.makeText(getApplicationContext(), "Signed in successfully "+ user.getEmail().toString(), Toast.LENGTH_LONG).show();
-                    Intent newIntent=new Intent(getApplicationContext(),mainPageActivity.class);
-                    startActivity(newIntent);
-
-
-                }
-                else {
-                    Log.d(TAG, "onAuthStateChanged: signed out");
-                    Toast.makeText(getApplicationContext(), "Signed out successfully", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-// Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });*/
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Attemting to add to db");
+
+
+                if (!TaskTitleTxt.getText().toString().equals(""))
+                {
                 String title=TaskTitleTxt.getText().toString();
                 String note=NoteTxt.getText().toString();
 
+                //getting task details
                 nTask=new Task();
-                nTask.setTitle(title);
                 nTask.set_id(new ObjectId());
+                nTask.setTitle(title);
                 nTask.setDescription(note);
 
+                //adding to local databse
                 MainActivity.thread.beginTransaction();
                 MainActivity.thread.copyToRealmOrUpdate(nTask);
                 MainActivity.thread.commitTransaction();
+
                 Toast.makeText(AddNewTaskActivity.this, "item inserted", Toast.LENGTH_SHORT).show();
 
-                String realmName = "My Project";
+                    MongoClient monClient= mUser.getMongoClient("mongodb-atlas");
+                    MongoDatabase monDb= monClient.getDatabase("ToDoListApp");
+                    MongoCollection<Document> mongoCollection=monDb.getCollection("Tasks");
 
-                SyncConfiguration config = new SyncConfiguration.Builder(MainActivity.monUser)
-                        .allowQueriesOnUiThread(true)
-                        .allowWritesOnUiThread(true)
-                        .build();
-
-                Realm backgroundThreadRealm = Realm.getInstance(config);
-
-                backgroundThreadRealm.executeTransaction (transactionRealm -> {
-                     transactionRealm.insert(nTask);
-                 });
-
-                syncv();
+                    LocalDate f= LocalDate.now();
+                    mongoCollection.insertOne(new Document("userID",mUser.getId()).append("note",title).append("date created",f.toString())).getAsync(result -> {
+                        if (result.isSuccess())
+                        {
+                            Log.d(TAG, "onClick: data inserted successfully");
+                            Toast.makeText(AddNewTaskActivity.this, "add new item db", Toast.LENGTH_LONG).show();
+                        }else
+                            Log.d(TAG, "onClick: data  "+result.getError().toString());
+                    });
 
 
-                if (!note.equals(""))
-                {
 
                 }
             }
@@ -179,7 +142,7 @@ void syncv() {
 
     // all Tasks in the realm
 
-    MainActivity.app= new App(new AppConfiguration.Builder(MainActivity.appid)
+  /*  MainActivity.app= new App(new AppConfiguration.Builder(MainActivity.appid)
             .defaultSyncClientResetStrategy(new DiscardUnsyncedChangesStrategy() {
                 @Override
                 public void onBeforeReset(Realm realm) {
@@ -218,7 +181,7 @@ void syncv() {
             }
         }
     });
-
+*/
 
 }}
 
